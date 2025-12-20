26ff9eaebbc27c54f77b6ce15ec0fca541259efb
Ken Gorman <gorman.kenneth@gmail.com>
2025-12-20 13:04:24 -0500

    modified selectable speeds

--- Kg.Velocity.Engine/SpeedPresets.cs
+++ Kg.Velocity.Engine/SpeedPresets.cs
@@ -9,11 +9,8 @@ public static class SpeedPresets
     {
         // Atmosphere
         new SpeedPreset { Name = "Walking", SpeedMph = 3, Group = "Atmosphere" },
-        new SpeedPreset { Name = "Horse", SpeedMph = 30, Group = "Atmosphere" },
-        new SpeedPreset { Name = "Wright Flyer", SpeedMph = 30, Group = "Atmosphere" },
         new SpeedPreset { Name = "Car (Highway)", SpeedMph = 65, Group = "Atmosphere" },
         new SpeedPreset { Name = "Boeing 747", SpeedMph = 570, Group = "Atmosphere" },
-        new SpeedPreset { Name = "Concorde", SpeedMph = 1354, Group = "Atmosphere" },
         new SpeedPreset { Name = "SR-71 Blackbird", SpeedMph = 2200, Group = "Atmosphere" },
 
         // Space
@@ -32,9 +29,9 @@ public static class SpeedPresets
 
         // FTL
         new SpeedPreset { Name = "2x Light Speed", SpeedMph = PhysicsConstants.SpeedOfLightMph * 2.0, Group = "FTL" },
-        new SpeedPreset { Name = "Warp 2", SpeedMph = PhysicsConstants.SpeedOfLightMph * 10.0, Group = "FTL" },
-        new SpeedPreset { Name = "Warp 4", SpeedMph = PhysicsConstants.SpeedOfLightMph * 100.0, Group = "FTL" },
-        new SpeedPreset { Name = "Warp 8", SpeedMph = PhysicsConstants.SpeedOfLightMph * 512.0, Group = "FTL" }
+        new SpeedPreset { Name = "10x Light Speed", SpeedMph = PhysicsConstants.SpeedOfLightMph * 10.0, Group = "FTL" },
+        new SpeedPreset { Name = "100x Light Speed", SpeedMph = PhysicsConstants.SpeedOfLightMph * 100.0, Group = "FTL" },
+        new SpeedPreset { Name = "1,000x Light Speed", SpeedMph = PhysicsConstants.SpeedOfLightMph * 1000.0, Group = "FTL" }
     };
 }
 
