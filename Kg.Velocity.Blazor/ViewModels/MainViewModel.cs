0953ba16cd948eebde71aa7922ad714e77bbc795
Ken Gorman <gorman.kenneth@gmail.com>
2026-01-09 23:38:20 -0500

    style changes around poster, removed download svg link

--- Kg.Velocity.Blazor/ViewModels/MainViewModel.cs
+++ Kg.Velocity.Blazor/ViewModels/MainViewModel.cs
@@ -170,7 +170,7 @@ public class MainViewModel
         ArrivalDateString = "—";
         ArrivalShipDateString = "—";
 
-        JourneySummary = "Calculating trip summary...";
+        JourneySummary = "Generating journey summary...";
         DisplayedJourneySummary = JourneySummary;
         PersonaName = "";
         PosterDataUrl = "";
